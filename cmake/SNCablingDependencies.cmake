f14dc130fd865f7cb3593f0c55843af1348c92f4
Francois Mauger <mauger@lpccaen.in2p3.fr>
2018-10-11 13:00:41 +0200

    preliminary Cmake support

---
+++ cmake/SNCablingDependencies.cmake
@@ -0,0 +1,93 @@
+# -----------------------------------------------------
+message( STATUS "[info] Define SNCabling dependencies")
+
+#-------------------------------------------------------
+# Bayeux
+#
+# Bayeux is the main external dependency, and we know it will additionally
+# search for and provide compatible versions of:
+#
+# - Boost
+# - GSL
+# - ROOT
+set(VIRE_BAYEUX_MIN_VERSION "3.4.0")
+message( STATUS "[info] Searching Bayeux ${VIRE_BAYEUX_MIN_VERSION}...")
+find_package(Bayeux ${VIRE_BAYEUX_MIN_VERSION} REQUIRED NO_MODULE)
+if (Bayeux_FOUND)
+  message( STATUS "[info] Bayeux ${Bayeux_VERSION} found.")
+  message( STATUS "[info] BAYEUX_BOOST_VERSION    = '${BAYEUX_BOOST_VERSION}'")
+  # Trick because BayeuxConfig 3.2 does not provide the BAYEUX_BOOST_COMPONENTS variable:
+  set(SNCabling_BAYEUX_VERSION ${Bayeux_VERSION})
+  # # Trick because BayeuxConfig 3.2 does not provide the BAYEUX_BOOST_COMPONENTS variable:
+  # set(BAYEUX_BOOST_COMPONENTS
+  #   "filesystem;system;serialization;iostreams;program_options;regex;thread"
+  #   )
+  message( STATUS "[info] BAYEUX_BOOST_COMPONENTS = '${BAYEUX_BOOST_COMPONENTS}'")
+  message( STATUS "[info] Bayeux_CMAKE_CONFIG_DIR = '${Bayeux_CMAKE_CONFIG_DIR}'")
+endif()
+
+# -------------------------------------------------------
+# - Boost:
+# Boost is also important with specific libs
+message( STATUS "[info] Searching special Boost material for SNCabling...")
+set(VIRE_BOOST_MIN_VERSION ${BAYEUX_BOOST_VERSION})
+set(Boost_NO_BOOST_CMAKE ON)
+set(VIRE_BOOST_COMPONENTS
+  ${BAYEUX_BOOST_COMPONENTS}
+  )
+list(APPEND VIRE_BOOST_COMPONENTS log)
+message( STATUS "[info] VIRE_BOOST_COMPONENTS   = '${VIRE_BOOST_COMPONENTS}'")
+message( STATUS "[info] Finding Boost with specific libraries...")
+find_package(Boost ${VIRE_BOOST_MIN_VERSION} REQUIRED
+  ${VIRE_BOOST_COMPONENTS}
+  )
+set(SNCabling_Boost_VERSION ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION})
+set(SNCabling_Boost_LIBRARIES)
+foreach(_vire_boost_lib ${SNCABLING_BOOST_COMPONENTS})
+  list(APPEND SNCabling_Boost_LIBRARIES Boost::${_vire_boost_lib})
+endforeach()
+message(STATUS "[info] SNCabling_Boost_VERSION   = '${SNCabling_Boost_VERSION}'")
+message(STATUS "[info] SNCabling_Boost_LIBRARIES = '${SNCabling_Boost_LIBRARIES}'")
+# message(STATUS "[info] Boost_LIBRARIES               = '${Boost_LIBRARIES}'")
+# message(STATUS "[info] Boost_FILESYSTEM_LIBRARY      = '${Boost_FILESYSTEM_LIBRARY}'")
+# message(STATUS "[info] Boost_SYSTEM_LIBRARY          = '${Boost_SYSTEM_LIBRARY}'")
+# message(STATUS "[info] Boost_SERIALIZATION_LIBRARY   = '${Boost_SERIALIZATION_LIBRARY}'")
+# message(STATUS "[info] Boost_IOSTREAMS_LIBRARY       = '${Boost_IOSTREAMS_LIBRARY}'")
+# message(STATUS "[info] Boost_PROGRAM_OPTIONS_LIBRARY = '${Boost_PROGRAM_OPTIONS_LIBRARY}'")
+# message(STATUS "[info] Boost_DATE_TIME_LIBRARY       = '${Boost_DATE_TIME_LIBRARY}'")
+# message(STATUS "[info] Boost_THREAD_LIBRARY          = '${Boost_THREAD_LIBRARY}'")
+# message(STATUS "[info] Boost_LOG_LIBRARY             = '${Boost_LOG_LIBRARY}'")
+# message(STATUS "[info] Boost_LOG_SETUP_LIBRARY       = '${Boost_LOG_SETUP_LIBRARY}'")
+
+#-----------------------------------------------------------------------
+
+#-----------------------------------------------------------------------
+# Qt5 support
+#
+if (SNCABLING_WITH_QT_GUI)
+  set(SNCablingLibrary_HEADERS_QT_TO_BE_MOCCED)
+  find_package(Qt5Core    ${SNCABLING_QT5_VERSION} REQUIRED)
+  find_package(Qt5Gui     ${SNCABLING_QT5_VERSION} REQUIRED)
+  find_package(Qt5Widgets ${SNCABLING_QT5_VERSION} REQUIRED)
+  find_package(Qt5Svg     ${SNCABLING_QT5_VERSION} REQUIRED)
+  message (STATUS "[info] Include dirs         = ${Qt5Core_INCLUDE_DIRS}")
+  message (STATUS "[info] Include private dirs = ${Qt5Core_PRIVATE_INCLUDE_DIRS}")
+  message (STATUS "[info] Include dirs         = ${Qt5Gui_INCLUDE_DIRS}")
+  message (STATUS "[info] Include private dirs = ${Qt5Gui_PRIVATE_INCLUDE_DIRS}")
+  message (STATUS "[info] Include dirs         = ${Qt5Widgets_INCLUDE_DIRS}")
+  message (STATUS "[info] Include private dirs = ${Qt5Widgets_PRIVATE_INCLUDE_DIRS}")
+  include_directories(
+    ${Qt5Core_INCLUDE_DIRS}
+    ${Qt5Core_PRIVATE_INCLUDE_DIRS}
+    ${Qt5Gui_INCLUDE_DIRS}
+    ${Qt5Gui_PRIVATE_INCLUDE_DIRS}
+    ${Qt5Widgets_INCLUDE_DIRS}
+    ${Qt5Widgets_PRIVATE_INCLUDE_DIRS}
+    )
+endif()
+
+
+#-----------------------------------------------------------------------
+message( STATUS "[info] End of SNCabling dependencies")
+
+# end
---
+++ tools/build.sh
@@ -0,0 +1,210 @@
+#!/usr/bin/env bash
+
+# A Bash script to build and install devel SNCabling on Ubuntu (1X.04).
+
+opwd=$(pwd)
+
+function my_exit()
+{
+    local error_code=$1
+    if [ "x${error_code}" = "x" ]; then
+	error_code=0
+    fi
+    shift 1
+    local error_msg="$@"
+    if [ ${error_code} -ne 0 -a -n "${error_msg}" ]; then
+	echo >&2 "[error] ${error_msg}"
+    fi
+    exit ${error_code}
+}
+
+function my_usage()
+{
+    cat<<EOF
+
+Options:
+
+   --help               : print help
+   --debug              : activate debug mode
+   --only-configure     : perform configuration stage only
+
+EOF
+    return
+}
+
+rebuild=0
+only_configure=0
+debug=0
+sncabling_source_dir="${opwd}"
+sncabling_version="develop"
+bayeux_version="3.4.0"
+install_dir=$(pwd)/_install.d
+build_dir=$(pwd)/_build.d
+with_tests=false
+
+function cl_parse()
+{
+    while [ -n "$1" ]; do
+	local arg="$1"
+	if [ "${arg}" = "--help" ]; then
+	    my_usage
+	    my_exit 0
+	elif [ "${arg}" = "--debug" ]; then
+	    debug=1
+	elif [ "${arg}" = "--with-tests" ]; then
+	    with_tests=true
+	elif [ "${arg}" = "--rebuild" ]; then
+	    rebuild=1
+	elif [ "${arg}" = "--only-configure" ]; then
+	    only_configure=1
+	elif [ "${arg}" = "--source-dir" ]; then
+	    shift 1
+	    sncabling_source_dir="$1"
+	elif [ "${arg}" = "--develop" ]; then
+	    sncabling_version="develop"
+	elif [ "${arg}" = "--bayeux-version" ]; then
+	    shift 1
+	    bayeux_version="$1"
+	fi
+	shift 1
+    done
+    return 0
+}
+
+echo >&2 "[info] SNCabling source dir : '${sncabling_source_dir}'"
+
+cl_parse $@
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] Command line parsing failed!"
+    my_exit 1
+fi
+
+# Check distribution:
+distrib_id=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d= -f2)
+distrib_release=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d= -f2)
+
+if [ "${distrib_id}" != "Ubuntu" ]; then
+    echo >&2 "[error] Not an Ubuntu Linux!"
+    my_exit 1
+fi
+
+if [ "${distrib_release}" != "16.04" -a "${distrib_release}" != "18.04" ]; then
+    echo >&2 "[error] Not an Ubuntu Linux version 16.04 or 18.04! Abort!"
+    my_exit 1
+else
+    echo >&2 "[info] Found Ubuntu Linux ${distrib_release}"
+fi
+
+# Check:
+which brew > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] Linuxbrew is not setup! Please run linuxbrew_setup! Abort!"
+    my_exit 1
+else
+    echo >&2 "[info] Found Linuxbrew : $(which brew)"
+fi
+
+if [ ! -d ${sncabling_source_dir} ]; then
+    echo >&2 "[error] SNCabling source directory '${sncabling_source_dir}' does not exist! Abort!"
+    my_exit 1
+fi
+
+# Checks:
+which bxquery > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] Bayeux is not setup!"
+    my_exit 1
+fi
+
+bxversion=$(bxquery --version)
+if [ "${bxversion}" != "${bayeux_version}" ]; then
+    echo >&2 "[error] Bayeux's version ${bxversion} is not supported!"
+    my_exit 1
+else
+    echo >&2 "[info] Found Bayeux ${bxversion}"
+fi
+
+which bxrabbitmq-query > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] BxRabbitMQ is not setup!"
+    my_exit 1
+else
+   echo >&2 "[info] Found BxRabbitMQ $(bxrabbitmq-query --version) in $(bxrabbitmq-query --cmakedir)"
+    
+fi
+
+which bxprotobuftools-query > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] BxProtobuftools is not setup!"
+    my_exit 1
+fi
+
+if [ -d ${install_dir} ]; then
+    rm -fr ${install_dir}
+fi
+
+if [ ! -d ${build_dir} ]; then
+    mkdir -p ${build_dir}
+else
+    if [ ${rebuild} -eq 1 ]; then
+	echo >&2 "[info] Rebuilding SNCabling..."
+	rm -fr ${build_dir}
+	mkdir -p ${build_dir}
+    fi
+fi
+
+linuxbrew_prefix=$(brew --prefix)
+echo >&2 "[info] Linuxbrew prefix : '${linuxbrew_prefix}'"
+
+cd ${build_dir}
+echo >&2 ""
+echo >&2 "[info] Configuring..."
+cmake \
+    -DCMAKE_BUILD_TYPE:STRING="Release" \
+    -DCMAKE_INSTALL_PREFIX:PATH="${install_dir}" \
+    -DCMAKE_FIND_ROOT_PATH:PATH="${linuxbrew_prefix}" \
+    -DBayeux_DIR:PATH="$(bxquery --cmakedir)" \
+    -DSNCABLING_COMPILER_ERROR_ON_WARNING=ON \
+    -DSNCABLING_CXX_STANDARD="11" \
+    -DSNCABLING_ENABLE_TESTING=ON \
+    -DSNCABLING_WITH_DOCS=ON \
+    -DSNCABLING_WITH_DEVELOPER_TOOLS=ON \
+    -GNinja \
+    ${sncabling_source_dir}
+if [ $? -ne 0 ]; then
+    echo >&2 "[error] SNCabling ${sncabling_version} configuration failed!"
+    my_exit 1
+fi
+
+if [ ${only_configure} -eq 0 ]; then
+
+    echo >&2 ""
+    echo >&2 "[info] Building..."
+    ninja -j4
+    if [ $? -ne 0 ]; then
+	echo >&2 "[error] SNCabling ${vire_version} build failed!"
+	my_exit 1
+    fi
+
+    if [ ${with_tests} = true ]; then
+	echo >&2 ""
+	echo >&2 "[info] Testing..."
+	ninja test
+	if [ $? -ne 0 ]; then
+	    echo >&2 "[error] SNCabling ${vire_version} testing failed!"
+	    my_exit 1
+	fi
+    fi
+
+    echo >&2 ""
+    echo >&2 "[info] Installing..."
+    ninja install
+    if [ $? -ne 0 ]; then
+	echo >&2 "[error] SNCabling ${vire_version} installation failed!"
+	my_exit 1
+    fi
+fi
+
+my_exit 0
+
+# end
