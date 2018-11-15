fd30e7c160f2e371408bac62c6dd2132ef23c5eb
Francois Mauger <mauger@lpccaen.in2p3.fr>
2018-11-15 11:34:51 +0100

    Update README

--- README.rst
+++ README.rst
@@ -12,13 +12,46 @@ Introduction
 ============
 
 This project  aims to describe the  cabling rules and schemes  for all
-submodules in  the SuperNEMO Demonstrator: calorimeter  signal cables,
-HV cables, tracker signal & HV cables, Light Injection System (LIS)...
+submodules in  the SuperNEMO Demonstrator: calorimeter  signal and HV cables,
+tracker signal & HV cables, Light Injection System (LIS)...
 
-Tools are also provided to generate  cabling tables and labels to help
+Expert tools (Python) are also provided to generate cabling tables and labels to help
 cabling operations at LSM.
 
+A C++ library, namely ``SNCabling`` is also built and provides a service
+which hosts all searchable cabling tables of interest.
 
+Example:
+
+.. code:: c++
+
+   #include <iostream>
+   #include <sncabling/service.h>
+   #include <sncabling/calo_signal_cabling.h>
+   #include <sncabling/om_id.h>
+   #include <sncabling/label.h>
+
+   int main() {
+	  // Instantiate and initialize the cabling service:
+	  sncabling::service snCabling;
+	  snCabling.initialize_simple();
+
+	  // Access to the calorimeter signal readout cabling map:
+	  const sncabling::calo_signal_cabling & caloSignalCabling = snCabling.get_calo_signal_cabling();
+
+	  // Search the readout channel associated to a given OM:
+	  sncabling::om_id calo_id(sncabling::OM_MAIN, 0, 3, 1);
+	  if (caloSignalCabling.has_om(calo_id)) {
+ 	  const sncabling::calo_signal_id & signal_channel = caloSignalCabling.get_channel(calo_id);
+	  std::cout << "OM [" << calo_id.to_label() << "] is associated to the WaveCatcher readout channel "
+	            << "[" << signal_channel.to_label() << "].\n";
+	  }
+
+	  // Terminate the cabling service:
+	  snCabling.reset();
+	  return 0;
+   }
+..
 
 
 
--- source/sncabling/testing/test-service.cxx
+++ source/sncabling/testing/test-service.cxx
@@ -6,8 +6,11 @@
 #include <sncabling/service.h>
 #include <sncabling/calo_hv_cabling.h>
 #include <sncabling/calo_signal_cabling.h>
+#include <sncabling/om_id.h>
+#include <sncabling/label.h>
 
 void run1();
+void run2();
 
 int main(void)
 {
@@ -16,6 +19,9 @@ int main(void)
 
     std::cerr << "[info] " << "run 1..." << std::endl;
     run1();
+
+    std::cerr << "[info] " << "run 2..." << std::endl;
+    run2();
 
   } catch (std::exception & err) {
     std::cerr << "[error] " << err.what() << std::endl;
@@ -32,10 +38,10 @@ void run1()
   datatools::properties cabling_config;
   cabling_config.store_path("logging.priority", "debug");
   // Force specific cabling maps:
-  // cabling_config.store_path("CaloHV.default_map",
-  //                           "${SNCABLING_RESOURCE_FILES_DIR}/snemo/demonstrator/cabling/CaloHV/0.3/CaloHVCabling/calohv_mapping.csv");
-  // cabling_config.store_path("CaloSignal.default_map",
-  //                           "${SNCABLING_RESOURCE_FILES_DIR}/snemo/demonstrator/cabling/CaloSignal/0.2/CaloSignalCabling/calosignal_mapping.csv");
+  cabling_config.store_path("CaloHV.default_map",
+                            "${SNCABLING_RESOURCE_FILES_DIR}/snemo/demonstrator/cabling/CaloHV/0.3/CaloHVCabling/calohv_mapping.csv");
+  cabling_config.store_path("CaloSignal.default_map",
+                            "${SNCABLING_RESOURCE_FILES_DIR}/snemo/demonstrator/cabling/CaloSignal/0.2/CaloSignalCabling/calosignal_mapping.csv");
 
   // Cabling service uses default cabling map (CaloHV, CaloSignal) if none are specified:
   cabling.initialize_standalone(cabling_config);
@@ -52,5 +58,27 @@ void run1()
   std::clog << std::endl;
   
   cabling.reset();
+  return;
+}
+
+void run2()
+{
+  // Instantiate and initialize the cabling service:
+  sncabling::service snCabling;
+  snCabling.initialize_simple(); // Using default maps
+
+  // Access to the calorimeter signal readout cabling map:
+  const sncabling::calo_signal_cabling & caloSignalCabling = snCabling.get_calo_signal_cabling();
+
+  // Search the readout channel associated to a given OM:
+  sncabling::om_id calo_id(sncabling::OM_MAIN, 0, 3, 1);
+  if (caloSignalCabling.has_om(calo_id)) {
+    const sncabling::calo_signal_id & signal_channel = caloSignalCabling.get_channel(calo_id);
+    std::cout << "OM [" << calo_id.to_label() << "] is associated to the WaveCatcher readout channel "
+              << "[" << signal_channel.to_label() << "].\n";
+  }
+
+  // Terminate the cabling service:
+  snCabling.reset();
   return;
 }
