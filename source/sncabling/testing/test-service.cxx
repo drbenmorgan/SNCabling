5df9eef911bf12fa3c3f6a02b9580d3018c9b4f3
Francois Mauger <mauger@lpccaen.in2p3.fr>
2019-09-18 17:50:01 +0200

    Update CaloHV tag in test

--- source/sncabling/testing/test-service.cxx
+++ source/sncabling/testing/test-service.cxx
@@ -42,7 +42,7 @@ void run2()
   cabling_config.store_path("logging.priority", "debug");
   // Force specific cabling maps:
   cabling_config.store_path("CaloHV.default_map",
-                            "${SNCABLING_RESOURCE_FILES_DIR}/config/snemo/demonstrator/cabling/CaloHV/tags/original/CaloHVCabling/calohv_mapping.csv");
+                            "${SNCABLING_RESOURCE_FILES_DIR}/config/snemo/demonstrator/cabling/CaloHV/tags/actual_1/CaloHVCabling/calohv_mapping.csv");
   cabling_config.store_path("CaloSignal.default_map",
                             "${SNCABLING_RESOURCE_FILES_DIR}/config/snemo/demonstrator/cabling/CaloSignal/tags/original/CaloSignalCabling/calosignal_mapping.csv");
 
